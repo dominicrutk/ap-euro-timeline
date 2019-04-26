039ca72ddb2f2d26a45b5537834fa5a71e1cd074
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2019-04-26 02:58:41 -0500

    Added no desc political misc events

--- events.js
+++ events.js
@@ -18,4 +18,147 @@ const events = {
     //         }
     //     ]
     // }
+    spanishArmada: {
+        name: 'Spanish Armada',
+        startYear: 1588,
+        endYear: 1588,
+        latitude: 51.139097,
+        longitude: 1.913333,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    thirtyYearsWar: {
+        name: 'Thirty Years\' War',
+        startYear: 1618,
+        endYear: 1684,
+        latitude: 50.090233,
+        longitude: 14.40164,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    warOfTheAustrianSuccession: {
+        name: 'War of the Austrian Succession',
+        startYear: 1740,
+        endYear: 1748,
+        latitude: 50.7753,
+        longitude: 6.0839,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    diplomaticRevolution: {
+        name: 'Diplomatic Revolution',
+        startYear: 1756,
+        endYear: 1756,
+        latitude: 48.8048,
+        longitude: 2.1203,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    sevenYearsWar: {
+        name: 'Seven Years\' War',
+        startYear: 1756,
+        endYear: 1763,
+        latitude: 48.8566,
+        longitude: 2.3522,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    napoleonicWars: {
+        name: 'Napoleonic Wars',
+        startYear: 1803,
+        endYear: 1815,
+        latitude: 50.680,
+        longitude: 4.412,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    congressOfVienna: {
+        name: 'Congress of Vienna',
+        startYear: 1814,
+        endYear: 1815,
+        latitude: 48.2082,
+        longitude: 16.3738,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    compromiseOf1867: {
+        name: 'Compromise of 1867',
+        startYear: 1867,
+        endYear: 1867,
+        latitude: 48.2082,
+        longitude: 16.3738,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    worldWarI: {
+        name: 'World War I',
+        startYear: 1914,
+        endYear: 1918,
+        latitude: 43.8563,
+        longitude: 18.4131,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    worldWarII: {
+        name: 'World War II',
+        startYear: 1939,
+        endYear: 1945,
+        latitude: 49.43,
+        longitude: 11.12,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    coldWar: {
+        name: 'Cold War',
+        startYear: 1947,
+        endYear: 1991,
+        latitude: 52.51611,
+        longitude: 13.37694,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    septemberEleventh: {
+        name: '9/11 Attacks',
+        startYear: 2001,
+        endYear: 2001,
+        latitude: 40.711667,
+        longitude: -74.013611,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
+    brexitVote: {
+        name: 'Brexit Vote',
+        startYear: 2016,
+        endYear: 2016,
+        latitude: 51.545194,
+        longitude: -2.571111,
+        category: 'political-diplomatic',
+        description: [
+
+        ]
+    },
 };
